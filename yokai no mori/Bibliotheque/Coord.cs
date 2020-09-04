061523a3753ef3c172e9a06ac600d1a697446a7b
Reblochon <Reblochon@users.noreply.github.com>
2020-09-04 19:56:26 +0200

    Posting the project
    It was working last time I tested it

---
+++ yokai no mori/Bibliotheque/Coord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque
+{
+    public class Coord
+    {
+        private int _x;
+        private int _y;
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+
+            set
+            {
+                _y = value;
+            }
+        }
+
+
+       
+    
+
+}
+   
+}
